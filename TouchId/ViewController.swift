7ed80c57c6e72630e5230c4241616e0fa66b5a45
Raja Bhaiya <rajabhaiya.niranjan@harman.com>
2017-09-06 15:24:37 +0530

    Initial Commit

---
+++ TouchId/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchId
+//
+//  Created by Niranjan, Rajabhaiya on 06/09/17.
+//  Copyright © 2017 harman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
